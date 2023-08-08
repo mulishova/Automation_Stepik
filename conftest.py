2213cc0017da482dacdb2859413f9ae69aff0985
Ekaterina Mulishova <mulishova.e@gmail.com>
2023-08-08 18:04:19 +0400

    conftest and gitignore added

---
+++ conftest.py
@@ -0,0 +1,34 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as OptionsChrome
+from selenium.webdriver.firefox.options import Options as OptionsFirefox
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",  # default=None
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="es",
+                     help="Choose language: en, es, ru...")
+
+
+@pytest.fixture(scope="function")  # “function” - default, “class”, “module”, “session”
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+    browser = None
+
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options = OptionsChrome()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        options = OptionsFirefox()
+        options.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(options=options)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
