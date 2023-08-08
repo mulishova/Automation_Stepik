2e8bcf2826972e8bca6366fac915f72acf5826b5
Ekaterina Mulishova <mulishova.e@gmail.com>
2023-08-08 18:10:09 +0400

    default language changed to 'en'

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ from selenium.webdriver.firefox.options import Options as OptionsFirefox
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",  # default=None
                      help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default="es",
+    parser.addoption('--language', action='store', default="en",
                      help="Choose language: en, es, ru...")
 
 
