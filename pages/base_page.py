9243a0ced43877edbdb7162af43d53c9341a2744
Ekaterina Mulishova <mulishova.e@gmail.com>
2023-08-11 14:25:48 +0400

    params added to test_guest_can_add_product_to_basket()

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,7 +2,7 @@ import math
 
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-
+import time
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
@@ -26,13 +26,13 @@ class BasePage():
         answer = str(math.log(abs((12 * math.sin(float(x))))))
         alert.send_keys(answer)
         alert.accept()
-        try:
-            alert = self.browser.switch_to.alert
-            alert_text = alert.text
-            print(f"Your code: {alert_text}")
-            alert.accept()
-        except NoAlertPresentException:
-            print("No second alert presented")
-            return False
+        #try:
+        #    alert = self.browser.switch_to.alert
+        #    alert_text = alert.text
+        #    print(f"Your code: {alert_text}")
+        #    alert.accept()
+        #except NoAlertPresentException:
+        #    print("No second alert presented")
+        #    return False
         return True
 
